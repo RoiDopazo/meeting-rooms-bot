d068f71c853737b3e8cf207526e4ec68d44564e1
Rodrigo Dopazo Iglesias <r.a.dopazo.iglesias@accenture.com>
2020-01-09 12:09:48 +0100

    added prettier and eslint

--- bot.js
+++ bot.js
@@ -1,79 +1,43 @@
 //  __   __  ___        ___
-// |__) /  \  |  |__/ |  |  
-// |__) \__/  |  |  \ |  |  
+// |__) /  \  |  |__/ |  |
+// |__) \__/  |  |  \ |  |
 
 // This is the main file for the meeting rooms bot bot.
 
 // Import Botkit's core features
-const { Botkit } = require('botkit');
-const { BotkitCMSHelper } = require('botkit-plugin-cms');
-
-// Import a platform-specific adapter for botframework.
-
-const { MongoDbStorage } = require('botbuilder-storage-mongodb');
+const { Botkit } = require('botkit')
 
 // Load process.env values from .env file
-require('dotenv').config();
-
-let storage = null;
-if (process.env.MONGO_URI) {
-    storage = mongoStorage = new MongoDbStorage({
-        url : process.env.MONGO_URI,
-    });
-}
-
-
-
-
+require('dotenv').config()
 
 const controller = new Botkit({
-    webhook_uri: '/api/messages',
+  webhook_uri: '/api/messages',
 
-    adapterConfig: {
-        appId: process.env.APP_ID,
-        appPassword: process.env.APP_PASSWORD,
-    },
-
-    storage
-});
-
-if (process.env.CMS_URI) {
-    controller.usePlugin(new BotkitCMSHelper({
-        uri: process.env.CMS_URI,
-        token: process.env.CMS_TOKEN,
-    }));
-}
+  adapterConfig: {
+    appId: process.env.APP_ID,
+    appPassword: process.env.APP_PASSWORD,
+  },
+})
 
 // Once the bot has booted up its internal services, you can use them to do stuff.
 controller.ready(() => {
+  // load traditional developer-created local custom feature modules
+  controller.loadModules(__dirname + '/features')
 
-    // load traditional developer-created local custom feature modules
-    controller.loadModules(__dirname + '/features');
+  /* catch-all that uses the CMS to trigger dialogs */
+  if (controller.plugins.cms) {
+    controller.on('message,direct_message', async (bot, message) => {
+      let results = false
+      results = await controller.plugins.cms.testTrigger(bot, message)
 
-    /* catch-all that uses the CMS to trigger dialogs */
-    if (controller.plugins.cms) {
-        controller.on('message,direct_message', async (bot, message) => {
-            let results = false;
-            results = await controller.plugins.cms.testTrigger(bot, message);
-
-            if (results !== false) {
-                // do not continue middleware!
-                return false;
-            }
-        });
-    }
-
-});
-
-
+      if (results !== false) {
+        // do not continue middleware!
+        return false
+      }
+    })
+  }
+})
 
 controller.webserver.get('/', (req, res) => {
-
-    res.send(`This app is running Botkit ${ controller.version }.`);
-
-});
-
-
-
-
-
+  res.send(`This app is running Botkit ${controller.version}.`)
+})
