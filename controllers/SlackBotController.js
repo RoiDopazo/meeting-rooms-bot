428b3208183f167a094dbbad7594edff6352f98c
Rodrigo Dopazo Iglesias <r.a.dopazo.iglesias@accenture.com>
2020-01-09 16:49:29 +0100

    removed unneded code

--- controllers/SlackBotController.js
+++ controllers/SlackBotController.js
@@ -51,36 +51,16 @@ const controller = new Botkit({
   adapter: adapter,
 })
 
-if (process.env.CMS_URI) {
-  controller.usePlugin(
-    new BotkitCMSHelper({
-      uri: process.env.CMS_URI,
-      token: process.env.CMS_TOKEN,
-    })
-  )
-}
-
 // Once the bot has booted up its internal services, you can use them to do stuff.
 controller.ready(() => {
   // load traditional developer-created local custom feature modules
   controller.loadModules(__dirname + '/../features')
-
-  /* catch-all that uses the CMS to trigger dialogs */
-  if (controller.plugins.cms) {
-    controller.on('message,direct_message', async (bot, message) => {
-      let results = false
-      results = await controller.plugins.cms.testTrigger(bot, message)
-
-      if (results !== false) {
-        // do not continue middleware!
-        return false
-      }
-    })
-  }
 })
 
 controller.webserver.get('/', (req, res) => {
-  res.send(`This app is running Botkit ${controller.version}.`)
+  res.send(
+    `(Slack Controller) This app is running Botkit ${controller.version}.`
+  )
 })
 
 controller.webserver.get('/install', (req, res) => {
