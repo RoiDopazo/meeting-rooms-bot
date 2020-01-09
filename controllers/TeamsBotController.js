bf9c8019ea6dfccc26e6c65760f0d4b0fad3208f
Rodrigo Dopazo Iglesias <r.a.dopazo.iglesias@accenture.com>
2020-01-09 12:34:07 +0100

    updated env variables

---
+++ controllers/TeamsBotController.js
@@ -0,0 +1,31 @@
+// Import Botkit's core features
+const { Botkit } = require('botkit')
+const {
+  MICROSOFT_TEAMS_URI,
+  MICROSOFT_TEAMS_APP_ID,
+  MICROSOFT_TEAMS_APP_PASSWORD,
+} = require('../env')
+
+// Load process.env values from .env file
+require('dotenv').config()
+
+const controller = new Botkit({
+  webhook_uri: MICROSOFT_TEAMS_URI,
+
+  adapterConfig: {
+    appId: MICROSOFT_TEAMS_APP_ID,
+    appPassword: MICROSOFT_TEAMS_APP_PASSWORD,
+  },
+})
+
+// Once the bot has booted up its internal services, you can use them to do stuff.
+controller.ready(() => {
+  // load traditional developer-created local custom feature modules
+  controller.loadModules(__dirname + '/../features')
+})
+
+controller.webserver.get('/', (req, res) => {
+  res.send(
+    `(Teams Controller) This app is running Botkit ${controller.version}.`
+  )
+})
---
+++ env.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+
+const MICROSOFT_TEAMS_APP_ID = ''
+const MICROSOFT_TEAMS_APP_PASSWORD = ''
+const MICROSOFT_TEAMS_URI = '/api/teams/messages'
+
+module.exports = {
+  MICROSOFT_TEAMS_APP_ID,
+  MICROSOFT_TEAMS_APP_PASSWORD,
+  MICROSOFT_TEAMS_URI,
+}
